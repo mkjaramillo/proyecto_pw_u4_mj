5f2b79287b3e298260d7667197479d4c0e1af8d6
mkjaramillo <85204686+mkjaramillo@users.noreply.github.com>
2022-09-03 20:19:35 -0500

    taller31

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,51 +2,137 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+      For a guide and recipes on how to configure / customize this project,<br />
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a
+      >.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+          >babel</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
+          target="_blank"
+          rel="noopener"
+          >unit-jest</a
+        >
+      </li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+          >Forum</a
+        >
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+          >Community Chat</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+          >Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      </li>
     </ul>
     <h3>Ecosystem</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+          >vue-router</a
+        >
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          target="_blank"
+          rel="noopener"
+          >vue-devtools</a
+        >
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+          >vue-loader</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+          >awesome-vue</a
+        >
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
-  import obtenerEmpleado from '@/helpers/procesarEmpleado';
-import { onMounted } from 'vue';
+import obtenerEmpleado from "@/helpers/procesarEmpleado";
+import insertarEmpleado from "@/helpers/procesarEmpleado";
+import actualizarEmpleado from "@/helpers/procesarEmpleado";
+import eliminarEmpleado from "@/helpers/procesarEmpleado";
+import { onMounted } from "vue";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
-  methods:{
-    async buscar(id){
-      await obtenerEmpleado(id)
-    }
+  methods: {
+    async buscar(id) {
+      await obtenerEmpleado(id);
+    },
+    async insertar(body) {
+      await insertarEmpleado(body);
+    },
+    async actualizar(body) {
+      await actualizarEmpleado(body);
+    },
+    async eliminar(id) {
+      await eliminarEmpleado(id);
+    },
   },
-  mounted(){
-    this.buscar(1)
-  }
-}
+  mounted() {
+    this.buscar(2);
+    const empl = {
+      id: 5,
+      nombre: "Patricia",
+      apellido: "Jaramillo",
+      fechaNacimiento: "2022-08-24T00:00:00",
+      salario: 250,
+    };
+    this.insertar(empl);
+
+    const empl2 = {
+      id: 2,
+      nombre: "Actualzado",
+      apellido: "Jaramillo",
+      fechaNacimiento: "2022-08-24T00:00:00",
+      salario: 250,
+    };
+    this.actualizar(empl2);
+   this.eliminar(3);
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/helpers/procesarEmpleado.js
+++ src/helpers/procesarEmpleado.js
@@ -1,11 +1,56 @@
-
+import axios from "axios";
+//metodos fachada a exportas
 const obtenerEmpleado=async(id)=>{
-return await obtenerEmpleadoAPI(id);
+return await obtenerEmpleadoAxios(id);
 }
+const insertarEmpleado=async(body)=>{
+   return await insertarEmpleadoAxios(body);
+   }
+   const actualizarEmpleado=async(body)=>{
+      return await actualizarEmpleadoAxios(body);
+   }
+   const eliminarEmpleado=async(id)=>{
+      return await eliminarEmpleadoAxios(id);
+   } 
+
+//metodos de consumo del api
 const obtenerEmpleadoAPI=async(id)=>{
    const data=await fetch(`http://localhost:8085/APINomina/V1/empleados/${id}`).then(r=> r.json())
    console.log(data)
    return data
 
 }
-export default obtenerEmpleado
+//get
+const obtenerEmpleadoAxios=async(id)=>{
+   const data = axios.get(`http://localhost:8085/APINomina/V1/empleados/${id}`).then(r=>r.data)
+   console.log(data)
+   return data 
+
+}
+//post
+const insertarEmpleadoAxios=async(body)=>{
+   //cabeceras
+   const data = axios.post(`http://localhost:8085/APINomina/V1/empleados`,body).then(r=> r.data)
+   console.log(data)
+   return data 
+
+
+}
+//put
+const actualizarEmpleadoAxios=async(body)=>{
+   //cabeceras
+   
+   const data = axios.put(`http://localhost:8085/APINomina/V1/empleados`,body).then(r=> r.data)
+   console.log(data)
+   return data 
+
+
+}
+//delete
+const eliminarEmpleadoAxios=async(id)=>{
+   const data = axios.delete(`http://localhost:8085/APINomina/V1/empleados/${id}`).then(r=>r.data)
+   console.log(data)
+   return data 
+
+}
+export default obtenerEmpleado;insertarEmpleado;actualizarEmpleado;eliminarEmpleado;
